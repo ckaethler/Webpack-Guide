703722b33d8552b6df46b476ad12267403575149
Cat Kaethler <59104730+ckaethler@users.noreply.github.com>
2020-10-26 19:04:45 -0400

    added image and webpack configuration to add image

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.(xml)$/,
+                use: [
+                    'xml-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
