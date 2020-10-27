b108af35c2dc30559cad542fdbe2f55b2046d369
Cat Kaethler <59104730+ckaethler@users.noreply.github.com>
2020-10-27 00:03:33 -0400

    created seperate dev and prod files for webpack & added webpack server

---
+++ dist/main.8e8d62a3831d99b1aced.hot-update.js
@@ -0,0 +1,35 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatewebpack_guide"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_hello_world_button_hello_world_button__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/hello-world-button/hello-world-button */ \"./src/components/hello-world-button/hello-world-button.js\");\n/* harmony import */ var _components_heading_heading__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/heading/heading */ \"./src/components/heading/heading.js\");\n\n\nvar heading = new _components_heading_heading__WEBPACK_IMPORTED_MODULE_1__.default();\nheading.render();\nvar helloWorldButton = new _components_hello_world_button_hello_world_button__WEBPACK_IMPORTED_MODULE_0__.default();\nhelloWorldButton.render();\n\nif (false) {} else if (true) {\n  console.log('development mode');\n}\n\nhelloWorldButton.methodThatDoesNotExist();\n\n//# sourceURL=webpack://webpack-guide/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "a442c9b14efc8912d476"
+/******/ 	})();
+/******/ 	
+/******/ }
+);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading';
 
+const heading  = new Heading();
+heading.render();
+
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
-const heading  = new Heading();
-heading.render();
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('development mode');
+}
+
+helloWorldButton.methodThatDoesNotExist();
